ed2336474cabc930bcfc55110a40c811354a9f30
SandeepOP1 <101936804+SandeepOP1@users.noreply.github.com>
2022-05-20 11:25:14 +0530

    Update EM 4.py

---
+++ EM 4.py
@@ -0,0 +1,172 @@
+
+
+import pandas as pd
+pass_data = pd.read_excel("D:/Assignments/Ensemble Model/Data/Ensemble_Password_Strength.xlsx")
+
+#creating dummies
+pass_data1 = pd.get_dummies(pass_data[['characters']])
+pass_data.drop(pass_data[['characters']], axis=1, inplace=True)
+pass_data = pd.concat([pass_data, pass_data1],axis=1)
+
+# Input and Output Split
+predictors = pass_data.loc[:, pass_data.columns!="characters_strength"]
+type(predictors)
+
+target = pass_data["characters_strength"]
+type(target)
+
+# Train Test partition of the data
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(predictors, target, test_size = 0.2, random_state=0)
+
+# GridSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+rf_clf_grid = RandomForestClassifier(n_estimators=500, n_jobs=1, random_state=42)
+
+param_grid = {"max_features": [4, 5, 6, 7, 8, 9, 10], "min_samples_split": [2, 3, 10]}
+
+grid_search = GridSearchCV(rf_clf_grid, param_grid, n_jobs = -1, cv = 5, scoring = 'accuracy')
+
+grid_search.fit(x_train, y_train)
+
+grid_search.best_params_
+
+cv_rf_clf_grid = grid_search.best_estimator_
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+confusion_matrix(y_test, cv_rf_clf_grid.predict(x_test))
+accuracy_score(y_test, cv_rf_clf_grid.predict(x_test))
+
+########### Bagging technique
+from sklearn import tree
+clftree = tree.DecisionTreeClassifier()
+from sklearn.ensemble import BaggingClassifier
+
+bag_clf = BaggingClassifier(base_estimator = clftree, n_estimators = 500,
+                            bootstrap = True, n_jobs = 1, random_state = 42)
+
+bag_clf.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Evaluation on Testing Data
+confusion_matrix(y_test, bag_clf.predict(x_test))
+accuracy_score(y_test, bag_clf.predict(x_test))
+
+# Evaluation on Training Data
+confusion_matrix(y_train, bag_clf.predict(x_train))
+accuracy_score(y_train, bag_clf.predict(x_train))
+
+########### AdaBoosting technique
+from sklearn.ensemble import AdaBoostClassifier
+
+ada_clf = AdaBoostClassifier(learning_rate = 0.02, n_estimators = 5000)
+
+ada_clf.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Evaluation on Testing Data
+confusion_matrix(y_test, ada_clf.predict(x_test))
+accuracy_score(y_test, ada_clf.predict(x_test))
+
+# Evaluation on Training Data
+confusion_matrix(y_train, ada_clf.predict(x_train))
+accuracy_score(y_train, ada_clf.predict(x_train))
+
+######## Grddient boosting
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+boost_clf = GradientBoostingClassifier()
+
+boost_clf.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Evaluation on Testing Data
+confusion_matrix(y_test, boost_clf.predict(x_test))
+accuracy_score(y_test, boost_clf.predict(x_test))
+
+# Evaluation on Training Data
+confusion_matrix(y_train, boost_clf.predict(x_train))
+accuracy_score(y_train, boost_clf.predict(x_train))
+
+# Hyperparameters
+boost_clf2 = GradientBoostingClassifier(learning_rate = 0.02, n_estimators = 1000, max_depth = 1)
+boost_clf2.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Evaluation on Testing Data
+confusion_matrix(y_test, boost_clf2.predict(x_test))
+accuracy_score(y_test, boost_clf2.predict(x_test))
+
+# Evaluation on Training Data
+confusion_matrix(y_train, boost_clf2.predict(x_train))
+accuracy_score(y_train, boost_clf2.predict(x_train))
+
+
+############# Voting technique
+# Import the required libraries
+
+from sklearn import linear_model, svm, neighbors, naive_bayes
+from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import accuracy_score
+
+# Instantiate the learners (classifiers)
+learner_1 = neighbors.KNeighborsClassifier(n_neighbors=5)
+learner_2 = linear_model.Perceptron(tol=1e-2, random_state=0)
+learner_3 = svm.SVC(gamma=0.001)
+
+# Instantiate the voting classifier
+voting = VotingClassifier([('KNN', learner_1),
+                           ('Prc', learner_2),
+                           ('SVM', learner_3)])
+
+# Fit classifier with the training data
+voting.fit(x_train, y_train)
+
+# Predict the most voted class
+hard_predictions = voting.predict(x_test)
+
+# Accuracy of hard voting
+print('Hard Voting:', accuracy_score(y_test, hard_predictions))
+
+# Soft Voting
+# Instantiate the learners (classifiers)
+learner_4 = neighbors.KNeighborsClassifier(n_neighbors = 5)
+learner_5 = naive_bayes.GaussianNB()
+learner_6 = svm.SVC(gamma = 0.001, probability = True)
+
+# Instantiate the voting classifier
+voting = VotingClassifier([('KNN', learner_4),
+                           ('NB', learner_5),
+                           ('SVM', learner_6)],
+                            voting = 'soft')
+
+# Fit classifier with the training data
+voting.fit(x_train, y_train)
+learner_4.fit(x_train, y_train)
+learner_5.fit(x_train, y_train)
+learner_6.fit(x_train, y_train)
+
+# Predict the most probable class
+soft_predictions = voting.predict(x_test)
+
+# Get the base learner predictions
+predictions_4 = learner_4.predict(x_test)
+predictions_5 = learner_5.predict(x_test)
+predictions_6 = learner_6.predict(x_test)
+
+# Accuracies of base learners
+print('L4:', accuracy_score(y_test, predictions_4))
+print('L5:', accuracy_score(y_test, predictions_5))
+print('L6:', accuracy_score(y_test, predictions_6))
+
+# Accuracy of Soft voting
+print('Soft Voting:', accuracy_score(y_test, soft_predictions))
+
